1af27eb5cd676562b176f75b70b288c1af07ffd6
이건영 <aa74477383@gmail.com>
2026-09-09 08:41:59 +0900

    Update cho2.c

--- cho2/cho2.c
+++ cho2/cho2.c
@@ -4,6 +4,8 @@
 // 작성자 : 2600099 손재건
 // **********************************************
 
+#define _CRT_SECURE_NO_WARNINGS // 보안오류방지
+#pragma warning(disable:6031)  // 리턴값관련 경고 방지
 #include <stdio.h>
 
 int main(void)
